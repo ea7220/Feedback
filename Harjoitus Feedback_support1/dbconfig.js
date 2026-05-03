00b373d19d6ffa19e606e9e4c60a0978b643a795
Tran Dinh <s2401130@edu.bc.fi>
2026-05-03 23:10:13 +0300

    salansanan vaihtamine dbconfig laitoin oman XD

--- Harjoitus Feedback_support1/dbconfig.js
+++ Harjoitus Feedback_support1/dbconfig.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2/promise");
 const db = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "B0p4oqz9",
+  password: "sun ei mun",
   database: "feedback",
 });
 
